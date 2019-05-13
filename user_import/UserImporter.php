c5675de35da5c0df1f7d23f9c62dcdcef1a1555c
Shaun Moss <shaun@astromultimedia.com>
2019-05-13 16:48:49 +1000

    Added methods for printing help text and connecting to the database and creating the users table.

--- user_import/UserImporter.php
+++ user_import/UserImporter.php
@@ -48,16 +48,38 @@ class UserImporter {
   private $showHelp = FALSE;
 
   /**
-   * Array with database access details.
+   * The database user.
    *
-   * @var array
+   * @var string
    */
-  private $db = [];
+  private $dbUser;
+
+  /**
+   * The database password.
+   *
+   * @var string
+   */
+  private $dbPassword;
+
+  /**
+   * The database host.
+   *
+   * @var string
+   */
+  private $dbHost;
+
+  /**
+   * Reference to the database itself.
+   *
+   * @var mysqli
+   */
+  private $db;
 
   /**
    * UserImporter constructor. Private for singleton pattern.
    */
-  private function __construct() {}
+  private function __construct() {
+  }
 
   /**
    * Get the instance of UserImporter.
@@ -80,9 +102,9 @@ class UserImporter {
     global $argv;
     global $argc;
 
-    for ($i = 0; $i < $argc; $i++) {
+    // Loop through args. Start at arg[1] so we skip the actual script name.
+    for ($i = 1; $i < $argc; $i++) {
       $arg = $argv[$i];
-      echo $arg . "\n";
 
       switch ($arg) {
         case '--file':
@@ -109,7 +131,7 @@ class UserImporter {
         case '-u':
           if ($i + 1 < $argc) {
             $i++;
-            $this->db['user'] = $argv[$i];
+            $this->dbUser = $argv[$i];
           }
           else {
             throw new Exception("Database user not specified.");
@@ -119,7 +141,7 @@ class UserImporter {
         case '-p':
           if ($i + 1 < $argc) {
             $i++;
-            $this->db['password'] = $argv[$i];
+            $this->dbPassword = $argv[$i];
           }
           else {
             throw new Exception("Database password not specified.");
@@ -129,7 +151,7 @@ class UserImporter {
         case '-h':
           if ($i + 1 < $argc) {
             $i++;
-            $this->db['host'] = $argv[$i];
+            $this->dbHost = $argv[$i];
           }
           else {
             throw new Exception("Database host not specified.");
@@ -148,13 +170,113 @@ class UserImporter {
   }
 
   /**
+   * Print the help text.
+   */
+  public function printHelpText() {
+    echo "Command line directives\n";
+    echo "\n";
+
+    echo "--file [csv_file_name]\n";
+    echo "Specify the path to the CSV file to be parsed. Required unless --help or --create_table is specified.\n";
+    echo "\n";
+
+    echo "--create_table\n";
+    echo "Create the database table only, without importing the file. Optional.\n";
+    echo "\n";
+
+    echo "--dry_run\n";
+    echo "Dry run. Read the CSV file and print the results, but don't insert data into the database. Optional.\n";
+    echo "\n";
+
+    echo "-u [username]\n";
+    echo "The database user name. Required unless --help, --create_table, or --dry_run is specified.\n";
+    echo "\n";
+
+    echo "-p [password]\n";
+    echo "The database password. Required unless --help, --create_table, or --dry_run is specified.\n";
+    echo "\n";
+
+    echo "-h [host]\n";
+    echo "The database host. Required unless --help, --create_table, or --dry_run is specified.\n";
+    echo "\n";
+
+    echo "--help\n";
+    echo "Display this help text (and do nothing else). Optional.\n";
+    echo "\n";
+  }
+
+  /**
+   * Connect to the database.
+   *
+   * @return \mysqli
+   *   The MySQLi database object.
+   *
+   * @throws \Exception
+   */
+  protected function connectDatabase(): mysqli {
+    // Check we have all the database parameters.
+    if (empty($this->dbUser)) {
+      throw new Exception("Database user not specified.");
+    }
+    if (empty($this->dbPassword)) {
+      throw new Exception("Database password not specified.");
+    }
+    if (empty($this->dbHost)) {
+      throw new Exception("Database host not specified.");
+    }
+
+    // Connect to the database table.
+    $this->db = new mysqli($this->dbHost, $this->dbUser, $this->dbPassword, 'catalyst');
+    if ($this->db->connect_error) {
+      throw new Exception($this->db->connect_error);
+    }
+
+    echo "Successful connection to database.\n";
+  }
+
+  /**
+   * Create or recreate the users table.
+   *
+   * @throws \Exception
+   */
+  protected function createUsersTable() {
+    // Drop table if it already exists.
+    echo "Creating users table.\n";
+    $result = $this->db->query("drop table if exists users");
+    if (!$result) {
+      throw new Exception("Error dropping users table: " . $this->db->error);
+    }
+
+    // Create or recreate table.
+    $result = $this->db->query("
+      create table users
+        email varchar(100) not null unique primary,
+        first_name varchar(100) not null,
+        last_name varchar(100) not null
+    ");
+    if (!$result) {
+      throw new Exception("Error creating users table: " . $this->db->error);
+    }
+  }
+
+  /**
    * Create the users table, read the users, and import into the database table
    * (depending on command line parameters).
+   *
+   * @throws \Exception
    */
   public function run() {
     if ($this->showHelp) {
-//      $this->printHelpText();
+      $this->printHelpText();
       return;
+    }
+
+    // Do we want to insert users into the database?
+    $insert_users = $this->createTableOnly || !$this->dryRun;
+
+    if ($insert_users) {
+      $this->db = $this->connectDatabase();
+      $this->createUsersTable();
     }
 
   }
